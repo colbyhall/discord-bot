280c95b2eb0441bc66288a480653d91a1f29a155
Colby Hall <Colby Hall>
2017-11-15 16:37:31 -0600

    ree

---
+++ src/commands/ree.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const Arguments = require('../types/arguments'); 
+const Message = Discord.Message;
+
+const utils = require('../utils');
+
+module.exports = {
+    name: 'ree',
+    category: 'laugh',
+    help: '`;ree` to meme',
+    /**
+     * @param {Message} message 
+     * @param {Arguments} args 
+     * @returns {Promise<Boolean>}
+     */
+    async execute(message, args) {
+
+        message.channel.send('**REEEEEE**', {
+            files: ['https://media.discordapp.net/attachments/329329501945331712/376505116993257475/4564575466.gif']
+        });
+
+        return false;
+    }
+}
